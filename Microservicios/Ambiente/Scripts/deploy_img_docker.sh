f8094324bcece2623d6ad01ba903a012ffbbbd11
pedrinchi1987 <61132686+pedrinchi1987@users.noreply.github.com>
2022-04-24 23:50:41 -0500

    cambio
    cambio

--- Microservicios/Ambiente/Scripts/deploy_img_docker.sh
+++ Microservicios/Ambiente/Scripts/deploy_img_docker.sh
@@ -2,7 +2,7 @@
 
 echo "------------------> INSTALAR IMAGEN DOCKER <------------------"
 
-sudo docker pull pedrinchi1987/nttda:454
-sudo docker run -p 5000:8080 pedrinchi1987/nttda:454
+sudo docker pull pedrinchi1987/nttda:459
+sudo docker run -p 5000:8080 pedrinchi1987/nttda:459
 
 echo "------------------> FIN IMAGEN DOCKER <------------------"
