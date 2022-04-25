b945b78a04feecc657120d83d1def1fc5eaae1db
pedrinchi1987 <61132686+pedrinchi1987@users.noreply.github.com>
2022-04-25 00:00:07 -0500

    prueba
    prueba

--- Microservicios/Ambiente/Scripts/deploy_img_docker.sh
+++ Microservicios/Ambiente/Scripts/deploy_img_docker.sh
@@ -2,7 +2,7 @@
 
 echo "------------------> INSTALAR IMAGEN DOCKER <------------------"
 
-sudo docker pull pedrinchi1987/nttda:459
-sudo docker run -p 5000:8080 pedrinchi1987/nttda:459
+sudo docker pull pedrinchi1987/nttda:{BUILD_ID}
+sudo docker run -p {PORT_MQ}:8080 pedrinchi1987/nttda:{BUILD_ID}
 
 echo "------------------> FIN IMAGEN DOCKER <------------------"
