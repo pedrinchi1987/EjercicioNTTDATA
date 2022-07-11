7004b4436cb16cf6632fe2153868e91b61899e27
pedrinchi1987 <pedrito1348@gmail.com>
2022-07-11 17:21:07 -0500

    cambio docker run

--- Microservicios/Ambiente/Scripts/deploy_img_docker.sh
+++ Microservicios/Ambiente/Scripts/deploy_img_docker.sh
@@ -3,6 +3,6 @@
 echo "------------------> INSTALAR IMAGEN DOCKER <------------------"
 
 sudo docker pull pedrinchi1987/nttda:{BUILD_ID}
-sudo docker run -d -p {PORT_MQ}:8080 pedrinchi1987/nttda:{BUILD_ID}
+sudo docker run -p {PORT_MQ}:8080 pedrinchi1987/nttda:{BUILD_ID}
 
 echo "------------------> FIN IMAGEN DOCKER <------------------"
